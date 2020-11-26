46472c49863adffb4d428510b969d3af0d0a382a
archiexdex <rc840327@gmail.com>
2020-11-26 09:20:43 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 # I guess I am root
-$USER=ntu
+USER=ntu
 
-# run jupyter
-su - $USER -c  jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' 
 # run ssh 
-/etc/init.d/ssh restart
+/etc/init.d/ssh start
+# run jupyter
+su - $USER -c  "jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''& "
